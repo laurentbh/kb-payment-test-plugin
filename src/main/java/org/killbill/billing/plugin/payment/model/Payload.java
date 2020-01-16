5b4edb72002e2e8386153d94975322eb148c24db
Laurent Buhler <lbuhler@groupon.com>
2020-01-16 14:42:06 -0800

    add METHODS in payload

--- src/main/java/org/killbill/billing/plugin/payment/model/Payload.java
+++ src/main/java/org/killbill/billing/plugin/payment/model/Payload.java
@@ -11,18 +11,23 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_ABSENT;
 public class Payload {
 
     private final String action;
+    private final String methods;
     private final int    sleepTime;
 
     @JsonCreator
     public Payload(@JsonProperty("CONFIGURE_ACTION") final String action,
+                   @JsonProperty("METHODS") final String methods,
                    @JsonProperty("SLEEP_TIME_SEC") final int sleepTime) {
         this.action = action;
+        this.methods = methods;
         this.sleepTime = sleepTime;
     }
 
     public String getAction() {
         return this.action;
     }
+
+    public String getMethods() { return this.methods;}
 
     public int getSeepTime() {
         return this.sleepTime;
--- src/main/java/org/killbill/billing/plugin/payment/resources/PaymentTestResource.java
+++ src/main/java/org/killbill/billing/plugin/payment/resources/PaymentTestResource.java
@@ -24,7 +24,6 @@ public class PaymentTestResource {
     @Path("status")
     @GET
     public void status() {
-        System.out.println(">?>>>>");
 
     }
 
